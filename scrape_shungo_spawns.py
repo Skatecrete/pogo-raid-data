6561b403998e422342f5219afc245d42d10e6863
Skatecrete <skatecrete@gmail.com>
2026-04-21 18:10:59 -0400

    Update scrape_shungo_spawns.py

--- scrape_shungo_spawns.py
+++ scrape_shungo_spawns.py
@@ -92,111 +92,171 @@ def fix_image_urls():
     
     # ===== COMPLETE FORM MAPPINGS =====
     form_mappings = {
-        # Castform
-        "Castform Rainy": "351_castform-rainy.webp",
-        "Castform Sunny": "351_castform-sunny.webp",
-        "Castform": "351_castformwebp",
-        
-        # Pumpkaboo
-        "Pumpkaboo Small": "710_pumpkaboo-small.webp",
-        "Pumpkaboo Average": "710_pumpkaboo-average.webp",
-        "Pumpkaboo Large": "710_pumpkaboo-large.webp",
-        "Pumpkaboo Super": "710_pumpkaboo-super.webp",
-        
-        # Deerling
-        "Deerling Spring Form": "585_deerling-spring.webp",
-        "Deerling Summer Form": "585-deerling-summer.webp",
-        "Deerling Autumn Form": "585-deerling-autumn.webp",
-        "Deerling Winter Form": "585-deerling-winter.webp",
-        
-        # Sawsbuck
-        "Sawsbuck Spring Form": "586_sawsbuck-spring.webp",
-        "Sawsbuck Summer Form": "586-sawsbuck-summer.webp",
-        "Sawsbuck Autumn Form": "586-sawsbuck-autumn.webp",
-        "Sawsbuck Winter Form": "586-sawsbuck-winter.webp",
-        
-        # Oricorio
-        "Oricorio Baile Style": "741_oricorio-baile-style.webp",
-        "Oricorio Pom-Pom Style": "741-oricorio-pom-pom-style.webp",
-        "Oricorio Sensu Style": "741_oricorio-sensu-style.webp",
-        
-        # Cherrim
-        "Cherrim Overcast Form": "421-cherrim-overcast.webp",
-        "Cherrim Sunny": "421_cherrim-sunshine.webp",
-        
-        # Burmy
-        "Burmy Plant Cloak": "412_burmy-plant-cloak.webp",
-        "Burmy Sandy Cloak": "412_burmy-sandy-cloak.webp",
-        "Burmy Trash Cloak": "412_burmy-trash-cloak.webp",
-        
-        # Flabébé
-        "Flabébé Blue Flower": "669_flab-b--blue-flower.webp",
-        "Flabébé Red Flower": "669_flab-b--red-flower.webp",
-        "Flabébé Yellow Flower": "669_flab-b--yellow-flower.webp",
-        "Flabébé White Flower": "669_flab-b--white-flower.webp",
-        "Flabébé Orange Flower": "669_flab-b--orange-flower.webp",
-        
-        # Floette
-        "Floette Blue Flower": "670_floette-blue-flower.webp",
-        "Floette Red Flower": "670_floette-red-flower.webp",
-        "Floette Yellow Flower": "670-floette-yellow-flower.webp",
-        
-        # Shellos/Gastrodon
-        "Shellos East Sea": "422_shellos-east-seawebp",
-        "Shellos West Sea": "422_shellos-west-seawebp",
-        "Gastrodon East Sea": "423_gastrodon-east-seawebp",
-        "Gastrodon West Sea": "423_gastrodon-west-seawebp",
-        
-        # Nidoran
-        "Nidoran♀": "29_nidoran-f.webp",
-        "Nidoran♂": "32_nidoran-m.webp",
-        
-        # Frillish
-        "Frillish Female": "592_frillish-female.webp",
-        "Frillish Male": "592_frillishwebp",
-        
-        # Oinkologne
-        "Oinkologne Female": "916_oinkologne-female.webp",
-        
-        # Alolan
-        "Rattata Alola": "19_rattata-alola.webp",
-        "Sandshrew Alola": "27_sandshrew-alola.webp",
-        "Vulpix Alola": "37_vulpix-alola.webp",
-        "Diglett Alola": "50_diglett-alola.webp",
-        "Meowth Alola": "52_meowth-alola.webp",
-        "Geodude Alola": "74_geodude-alola.webp",
-        "Grimer Alola": "88_grimer-alola.webp",
-        
-        # Galarian
-        "Meowth Galarian": "52_meowth-galarian.webp",
-        "Ponyta Galarian": "77_ponyta-galarian.webp",
-        "Slowpoke Galarian": "79_slowpoke-galarian.webp",
-        "Zigzagoon Galarian": "263_zigzagoon-galarian.webp",
-        
-        # Hisuian
-        "Growlithe Hisuian": "58_growlithe-hisuian.webp",
-        "Voltorb Hisuian": "100_voltorb-hisuian.webp",
-        "Sneasel Hisuian": "215_sneasel-hisuian.webp",
-        
-        # Paldean
-        "Wooper Paldea": "194_wooper-paldea.webp",
-        "Clodsire": "980_clodsirewebp",
-        
-        # Rotom
-        "Rotom Heat": "479_rotom-heat.webp",
-        "Rotom Wash": "479_rotom-wash.webp",
-        
-        # Other
-        "Farfetch'd": "83_farfetch-d.webp",
-        "Mr. Mime": "122_mr-mime.webp",
-        "Porygon": "137_porygonwebp",
-        "Porygon2": "233_porygon2webp",
-        "Porygon-Z": "474_porygon-zwebp",
-        "Ho-Oh": "250_ho-ohwebp",
-        "Mime Jr.": "439_mime-jrwebp",
-        "Sirfetch'd": "865_sirfetchdwebp",
-        "Mr. Rime": "866_mr-rimewebp",
-    }
+    # ===== Castform =====
+    "Castform Rainy": "351_castform-rainy.webp",
+    "Castform Sunny": "351_castform-sunny.webp",
+    "Castform Snowy": "351-castform-snowy.webp",
+    "Castform": "351_castformwebp",
+    
+    # ===== Pumpkaboo Sizes =====
+    "Pumpkaboo Small": "710_pumpkaboo-small.webp",
+    "Pumpkaboo Average": "710_pumpkaboo-average.webp",
+    "Pumpkaboo Large": "710_pumpkaboo-large.webp",
+    "Pumpkaboo Super": "710_pumpkaboo-super.webp",
+    "Pumpkaboo": "710_pumpkaboo.webp",
+    
+    # ===== Deerling Seasons =====
+    "Deerling Spring Form": "585_deerling-spring.webp",
+    "Deerling Summer Form": "585_deerling-summer.webp",
+    "Deerling Autumn Form": "585_deerling-autumn.webp",
+    "Deerling Winter Form": "585_deerling-winter.webp",
+    "Deerling Spring": "585_deerling-spring.webp",
+    "Deerling Summer": "585-deerling-summer.webp",
+    "Deerling Autumn": "585-deerling-autumn.webp",
+    "Deerling Winter": "585-deerling-winter.webp",
+    
+    # ===== Sawsbuck Seasons =====
+    "Sawsbuck Spring Form": "586_sawsbuck-spring.webp",
+    "Sawsbuck Summer Form": "586-sawsbuck-summer.webp",
+    "Sawsbuck Autumn Form": "586-sawsbuck-autumn.webp",
+    "Sawsbuck Winter Form": "586-sawsbuck-winter.webp",
+    "Sawsbuck Spring": "586_sawsbuck-spring.webp",
+    "Sawsbuck Summer": "586-sawsbuck-summer.webp",
+    "Sawsbuck Autumn": "586-sawsbuck-autumn.webp",
+    "Sawsbuck Winter": "586-sawsbuck-winter.webp",
+    
+    # ===== Oricorio Styles =====
+    "Oricorio Baile Style": "741_oricorio-baile-style.webp",
+    "Oricorio Pom-Pom Style": "741-oricorio-pom-pom-style.webp",
+    "Oricorio Pa'u Style": "741-oricorio-pau-style.webp",
+    "Oricorio Sensu Style": "741_oricorio-sensu-style.webp",
+    
+    # ===== Cherrim Forms =====
+    "Cherrim Overcast Form": "421-cherrim-overcast.webp",
+    "Cherrim Sunny": "421_cherrim-sunshine.webp",
+    "Cherrim Overcast": "421-cherrim-overcast.webp",
+    "Cherrim Sunshine": "421_cherrim-sunshine.webp",
+    
+    # ===== Burmy Cloaks =====
+    "Burmy Plant Cloak": "412_burmy-plant-cloak.webp",
+    "Burmy Sandy Cloak": "412_burmy-sandy-cloak.webp",
+    "Burmy Trash Cloak": "412_burmy-trash-cloak.webp",
+    
+    # ===== Flabébé Flowers =====
+    "Flabébé Blue Flower": "669_flab-b--blue-flower.webp",
+    "Flabébé Red Flower": "669_flab-b--red-flower.webp",
+    "Flabébé Yellow Flower": "669_flab-b--yellow-flower.webp",
+    "Flabébé White Flower": "669_flab-b--white-flower.webp",
+    "Flabébé Orange Flower": "669_flab-b--orange-flower.webp",
+    
+    # ===== Floette Flowers =====
+    "Floette Blue Flower": "670_floette-blue-flower.webp",
+    "Floette Red Flower": "670_floette-red-flower.webp",
+    "Floette Yellow Flower": "670-floette-yellow-flower.webp",
+    "Floette White Flower": "670-floette-white-flower.webp",
+    "Floette Orange Flower": "670-floette-orange-flower.webp",
+    "Floette Eternal Flower": "670-floette-eternal-flower.webp",
+    
+    # ===== Shellos / Gastrodon =====
+    "Shellos East Sea": "422_shellos-east-seawebp",
+    "Shellos West Sea": "422_shellos-west-seawebp",
+    "Gastrodon East Sea": "423_gastrodon-east-seawebp",
+    "Gastrodon West Sea": "423_gastrodon-west-seawebp",
+    
+    # ===== Nidoran Gender =====
+    "Nidoran♀": "29_nidoran-f.webp",
+    "Nidoran♂": "32_nidoran-m.webp",
+    
+    # ===== Frillish Gender =====
+    "Frillish Female": "592_frillish-female.webp",
+    "Frillish Male": "592_frillishwebp",
+    
+    # ===== Oinkologne Gender =====
+    "Oinkologne Female": "916_oinkologne-female.webp",
+    "Oinkologne Male": "916-oinkologne-male.webp",
+    
+    # ===== Alolan Forms =====
+    "Rattata Alola": "19_rattata-alola.webp",
+    "Raticate Alola": "20_raticate-alola.webp",
+    "Sandshrew Alola": "27_sandshrew-alola.webp",
+    "Sandslash Alola": "28_sandslashwebp",
+    "Vulpix Alola": "37_vulpix-alola.webp",
+    "Ninetales Alola": "38_ninetaleswebp",
+    "Diglett Alola": "50_diglett-alola.webp",
+    "Dugtrio Alola": "51_dugtrio-alola.webp",
+    "Meowth Alola": "52_meowth-alola.webp",
+    "Persian Alola": "53_persian-alola.webp",
+    "Geodude Alola": "74_geodude-alola.webp",
+    "Graveler Alola": "75_graveler-alola.webp",
+    "Grimer Alola": "88_grimer-alola.webp",
+    "Muk Alola": "89_muk-alola.webp",
+    "Exeggutor Alola": "103_exeggutorwebp",
+    "Marowak Alola": "105_marowakwebp",
+    "Raichu Alola": "26_raichuwebp",
+    
+    # ===== Galarian Forms =====
+    "Meowth Galarian": "52_meowth-galarian.webp",
+    "Ponyta Galarian": "77_ponyta-galarian.webp",
+    "Rapidash Galarian": "78_rapidash-galarian.webp",
+    "Slowpoke Galarian": "79_slowpoke-galarian.webp",
+    "Slowbro Galarian": "80_slowbrowebp",
+    "Farfetch'd Galarian": "83-farfetch-d-galarian.webp",
+    "Zigzagoon Galarian": "263_zigzagoon-galarian.webp",
+    "Linoone Galarian": "264_linoone-galarian.webp",
+    "Yamask Galarian": "562_yamask-galarian.webp",
+    "Stunfisk Galarian": "618_stunfisk-galarian.webp",
+    "Darumaka Galarian": "554_darumaka-galarian.webp",
+    "Corsola Galarian": "222_corsolawebp",
+    
+    # ===== Hisuian Forms =====
+    "Growlithe Hisuian": "58_growlithe-hisuian.webp",
+    "Arcanine Hisuian": "59_arcanine-hisuian.webp",
+    "Voltorb Hisuian": "100_voltorb-hisuian.webp",
+    "Electrode Hisuian": "101_electrode-hisuian.webp",
+    "Typhlosion Hisuian": "157_typhlosionwebp",
+    "Sneasel Hisuian": "215_sneasel-hisuian.webp",
+    "Sliggoo Hisuian": "705_sliggoo-hisuian.webp",
+    "Goodra Hisuian": "706_goodra-hisuian.webp",
+    "Avalugg Hisuian": "713_avalugg-hisuian.webp",
+    "Decidueye Hisuian": "724_decidueyewebp",
+    "Samurott Hisuian": "503_samurottwebp",
+    "Lilligant Hisuian": "549_lilligant-hisuian.webp",
+    "Zoroark Hisuian": "571_zoroark-hisuian.webp",
+    "Braviary Hisuian": "628_braviarywebp",
+    
+    # ===== Paldean Forms =====
+    "Wooper Paldea": "194_wooper-paldea.webp",
+    "Clodsire": "980_clodsirewebp",
+    
+    # ===== Rotom Forms =====
+    "Rotom Heat": "479_rotom-heat.webp",
+    "Rotom Wash": "479_rotom-wash.webp",
+    "Rotom Frost": "479_rotom-frost.webp",
+    "Rotom Fan": "479_rotom-fan.webp",
+    "Rotom Mow": "479_rotom-mow.webp",
+    
+    # ===== Darmanitan =====
+    "Darmanitan Standard": "555-darmanitan-standard-mode.webp",
+    "Darmanitan Zen": "555-darmanitan-zen-mode.webp",
+    "Darmanitan Galarian Standard": "555-darmanitan-galarian-zen-mode.webp",
+    "Darmanitan Galarian Zen": "555-darmanitan-galarian-zen-mode.webp",
+    
+    # ===== Porygon Family =====
+    "Porygon": "137_porygonwebp",
+    "Porygon2": "233_porygon2webp",
+    "Porygon-Z": "474_porygon-zwebp",
+    
+    # ===== Legendaries =====
+    "Ho-Oh": "250_ho-ohwebp",
+    
+    # ===== Special =====
+    "Type: Null": "772_type-nullwebp",
+    "Mime Jr.": "439_mime-jrwebp",
+    "Sirfetch'd": "865_sirfetchdwebp",
+    "Mr. Rime": "866_mr-rimewebp",
+    "Farfetch'd": "83_farfetch-d.webp",
+    "Mr. Mime": "122_mr-mime.webp",
+}
     
     updated_count = 0
     
